2ed21ac35488cdda5b2cf85957ebd43b31df90e7
Virgin Bitton <virgin.bitton@gmail.com>
2020-04-22 22:42:15 +0200

    first commit

---
+++ application/routes.py
@@ -0,0 +1,9 @@
+from flask import current_app as app
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/test")
+def test():
+    return "Hello test!"
---
+++ config.py
@@ -0,0 +1,20 @@
+"""Flask config class."""
+import os
+
+
+class Config:
+    """Base config vars."""
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME')
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
