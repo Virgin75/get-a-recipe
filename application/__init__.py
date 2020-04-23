32580aac21088a89efec448ac3f8b86ef6e16efc
Virgin Bitton <virgin.bitton@gmail.com>
2020-04-23 22:12:31 +0200

    mise en place structure de l'app

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+import json
+
+with open("data1.json", "r") as file:
+    db = json.load(file)
+    
 
 def create_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-
     with app.app_context():
         # Include our Routes
         from . import routes
+        from . import data
         
         return app
